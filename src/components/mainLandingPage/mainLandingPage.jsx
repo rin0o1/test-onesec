13e5c4f783b493bef3f7e143d56c696786c45db6
Timon-star-max <superdev112317@gmail.com>
2024-05-08 22:45:47 +0900

    final

--- src/components/mainLandingPage/mainLandingPage.jsx
+++ src/components/mainLandingPage/mainLandingPage.jsx
@@ -11,7 +11,7 @@ const MainLandingPage = () => {
       <div className="main-heading">
       <p>
         <h2>
-          Welcome Tomon!
+          Welcome Tomon! Vercel
         </h2>
         <h2 id="focus-txt">
           Celebrate your victory!
