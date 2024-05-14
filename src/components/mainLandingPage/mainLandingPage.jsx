f79b644cc3ecfcd985a521f41ec908e07c861a00
Timon-star-max <superdev112317@gmail.com>
2024-05-14 09:46:45 +0900

    test8

--- src/components/mainLandingPage/mainLandingPage.jsx
+++ src/components/mainLandingPage/mainLandingPage.jsx
@@ -11,7 +11,7 @@ const MainLandingPage = () => {
       <div className="main-heading">
       <p>
         <h2>
-          Welcome to Onesec!
+          Welcome
         </h2>
         <h2 id="focus-txt">
           You are a winner!
