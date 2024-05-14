95c3023431aa0a19f1dafbea8b8d15d38bfe27dc
Timon-star-max <superdev112317@gmail.com>
2024-05-14 10:51:48 +0900

    test14

--- src/components/mainLandingPage/mainLandingPage.jsx
+++ src/components/mainLandingPage/mainLandingPage.jsx
@@ -11,7 +11,7 @@ const MainLandingPage = () => {
       <div className="main-heading">
       <p>
         <h2>
-          Welcome Team1!
+          Welcome Team!
         </h2>
         <h2 id="focus-txt">
           You are a winner!
