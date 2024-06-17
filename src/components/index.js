bd3d6bdd64b4f848e792fb0da5f32a96bde52dcb
FranLin <francescorinaldidev@gmail.com>
2024-06-17 09:16:10 +0100

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export {default as MainPageLanding} from './mainLandingPage/mainLandingPage';
 export {default as LandingFooter} from './landingPageFooter/footer';
 export {default as BuyCoin} from './BuyCoin/BuyCoin';
 export {default as NoWallet} from './NoWallet/noWallet';
-export {default as ConnectWallet} from './connectWallet/connectWallet';
+export {default as ConnectWallet} from './connectWallet/connectWallet'; 
